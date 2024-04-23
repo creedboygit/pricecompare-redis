f5753c12eb8fbe71453b70c9ac5baa7a0d970812
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-04-23 21:30:33 +0900

    가격 비교 redis 적용 init

---
+++ src/test/java/com/valletta/pricecompareredis/PricecompareRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valletta.pricecompareredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricecompareRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
